66cf547ae83af47711ea380b06d27e585a5a6ade
NuRelic <bnowlin6@gmail.com>
2016-02-09 19:19:30 -0600

    BoonGroupSolution
    From interview 2/9/16

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using MyApp;
+using static System.Text.ASCIIEncoding;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzer obj = new FizzBuzzer();
+            
+            // In this method we give the function two lists that are assumed to be equal length and have index-relationships (ex. 2 => Fizz looking below
+            // The return value is a byte array to make the transfer between the method and any other source as faster than a List or other containers. 
+            int[] keys = new int[] { 2, 4, 9, 12, 15 };
+            string[] values = new string[] { "Fizz", "Buzz", "Jump", "Skip", "Roll" };
+            // long a = GC.GetTotalMemory(true);
+            byte[] results = obj.processSnapCrackles(5, 300, keys, values);
+            // long b = GC.GetTotalMemory(true);
+            string value = Encoding.ASCII.GetString(results);
+            foreach (string str in value.Split(' '))
+            {
+                Console.WriteLine(str);
+            }
+            // Console.WriteLine(a);
+            // Console.WriteLine(b);
+            Console.ReadLine();
+
+            // Slower Method
+            /*List<KeyValuePair<int,string>> testTuplesList = new List<KeyValuePair<int, string>>();
+            testTuplesList.Add(new KeyValuePair<int, string>(1, "Snap"));
+            testTuplesList.Add(new KeyValuePair<int, string>(2, "Crackle"));
+            testTuplesList.Add(new KeyValuePair<int, string>(4, "Pop"));
+            testTuplesList.Add(new KeyValuePair<int, string>(6, "Roll"));
+            testTuplesList.Add(new KeyValuePair<int, string>(7, "Jump"));*/
+
+        }
+
+    }
+}
---
+++ ConsoleApplication3/SnapCrackle/FizzBuzzer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyApp
+{
+    public class FizzBuzzer
+    {
+        // Requirements:
+        // Print 1 - 100 
+        // If % 3 == 0 print Number and Snap
+        // If % 5 == 0 print Number and Crackle
+        // If % 3 == 0 % 5 == 0 print Number and SnapCrackle
+        // Make useable by other programs, whatever they may be.
+        // Pass own range
+        // Remove Console.WriteLine -- Done
+        // Consider Memory -- Done (byte array returned for easy transport)
+        // Specify Denominators  -- key/values passed in, the commented out code below takes List<KVP<int,string>>
+        // Many Denominators -- This solution is scalable
+        // Naming Convention -- Made naming convention a little better
+        // Put on GitHuB -- 
+        public byte[] processSnapCrackles(int lowerBound, int upperBound, int[] keys, string[] values)
+        {
+            string str = "";
+            // Go through the range
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                str += i;
+                // After adding the number, cycle through all the keys to populate the "values" onto the string
+                for (int j = 0; j < keys.Length; j++)
+                {
+                    if (i%keys[j] == 0)
+                    {
+                        str += values[j];
+                    }
+                }
+                str += " ";
+            }
+            // Encode the string to bytes to transfer lightly/easily
+            byte[] results = Encoding.ASCII.GetBytes(str);
+            return results;
+        }
+        //Slower method
+
+        //public byte[] processSnapCrackless(int lowerBound, int upperBound, List<KeyValuePair<int, string>> denomsList)
+        //{
+        //    string str = "";
+        //    int stabalizer = 0;
+        //    for (int i = lowerBound; i <= upperBound; i++)
+        //    {
+        //        str += i + " ";
+        //        foreach (KeyValuePair<int, string> kvp in denomsList)
+        //        {
+        //            if (i % kvp.Key == 0)
+        //                str += kvp.Value;
+        //        }
+        //        str += " ";
+        //    }
+        //    byte[] results = Encoding.ASCII.GetBytes(str);
+        //    return results;
+        //}
+    }
+}
